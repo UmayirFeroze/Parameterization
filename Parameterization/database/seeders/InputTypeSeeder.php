1d064aaef240e4d7da5623866f6714509e72d84c
Umair Feroze <52625821+UmayirFeroze@users.noreply.github.com>
2021-08-04 15:49:37 +0530

    Input Types
    Input Type Model
    Database Migration
    Database Seeding

---
+++ Parameterization/database/seeders/InputTypeSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\InputType;
+use Illuminate\Database\Seeder;
+
+class InputTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Input Seeder
+        $inputTypes = [
+            0 => ["name" => "text", "type" => "text"],
+            1 => ["name" => "integer", "type" => "number", "min" => 0, "max" => 9999],
+            2 => ["name" => "percentage", "type" => "number", "min" => 0, "max" => 100, "step" => 0.2],
+        ];
+
+        foreach ($inputTypes as $key => $value) {
+            InputType::create([
+                'name' => $value['name'],
+                'type' => $value['type'],
+                'min' => (array_key_exists('min', $value)) ? $value['min'] : null,
+                'max' => (array_key_exists('max', $value)) ? $value['max'] : null,
+                'step' => (array_key_exists('step', $value)) ? $value['step'] : null,
+            ]);
+        }
+    }
+}
