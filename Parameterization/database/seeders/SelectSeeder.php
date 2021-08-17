38cc0d97a41db38f75b3194c5821cb25f55d5fdc
Umair Feroze <52625821+UmayirFeroze@users.noreply.github.com>
2021-08-17 11:17:31 +0530

    Globalize Import CSV Function
    App\Helper\Helper

--- Parameterization/database/seeders/SelectSeeder.php
+++ Parameterization/database/seeders/SelectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use App\Models\Select;
+use App\Helper\Helper;
 
 class SelectSeeder extends Seeder
 {
@@ -17,30 +18,9 @@ class SelectSeeder extends Seeder
     {
 
         DB::table('selects')->delete();
-
-        function import_csv($filename, $delimiter = ',')
-        {
-            if (!file_exists($filename) || !is_readable($filename))
-                return false;
-
-            $header = null;
-            $data = array();
-            if (($handle = fopen($filename, 'r')) !== false)
-            {
-                while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
-                {
-                    if (!$header)
-                        $header = $row;
-                    else
-                        $data[] = array_combine($header, $row);
-                }
-                fclose($handle);
-            }
-            return $data;
-        }
         
         $path = public_path('Seeders\selects.csv');
-        $records = import_csv($path);
+        $records = Helper::import_csv($path);
 
         foreach ($records as $key => $record) {
             Select::create([
